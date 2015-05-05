6dbe9e6d6d0aee2ba48e6e204ecbfc9c83916528
danielmonr <danielmonr@gmail.com>
2015-05-05 18:35:18 -0500

    first commit, main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  proyecto_so
+//
+//  Created by:
+//  Daniel Monzalvo A01021514
+//  Marcos Lopez A01020023
+//  on 05/05/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
