c00f46d582b0bb2369e966774818a88f9f72b44a
danielmonr <danielmonr@gmail.com>
2015-05-12 17:15:06 -0500

    fin

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 //
 
 #include <iostream>
+#include <vector>
 #include <string>
 #include "pthread.h"
 
@@ -23,14 +24,16 @@ void * LRU_bit(void *);
 
 int buscarEnArreglo(int *, int, int);
 int maximo(int *, int);
+int minimo(int *, int);
 int calcularDistancia(int *, int *, int);
 void avanzarDistancia(int * d, int tamano,int avance);
 
 using namespace std;
 
+
 int * numeros;
 int * fin_numeros;
-int size = 0;
+int tamo = 0;
 
 int main(int argc, const char * argv[]) {
     funciones funcion[5];
@@ -39,22 +42,32 @@ int main(int argc, const char * argv[]) {
     funcion[2] = LRU_bit;
     funcion[3] = LRU_cont;
     
-    
+    vector<int> provisional;
     
     // Leer numeros desde std input
     string a;
     int num;
     while(cin){
         getline(cin, a);
+        cout << a << endl;
         num = stoi(a);
-        numeros[size] = num;
-        ++ size;
+        provisional.push_back(num);
     }
-    fin_numeros = numeros + size;
+    
+    numeros = new int[provisional.size()];
+    int* indice = numeros;
+    for(auto item:provisional){
+        *indice = item;
+        indice++;
+    }
+    tamo = provisional.size();
+    
+    
+    // fin_numeros = numeros + size;
     
     // Crear threads para los distintos algoritmos
     
-    pthread_t threads[NUM_THREADS];
+    /* pthread_t threads[NUM_THREADS];
     int rc;
     for (int i = 0; i < NUM_THREADS; i++){
         rc = pthread_create(&threads[i], NULL, funcion[i], 0);
@@ -62,6 +75,7 @@ int main(int argc, const char * argv[]) {
             cout<< "No se pudo crear el thread " << i << "\n";
         }
     }
+     */
     return 0;
 }
 
@@ -78,7 +92,7 @@ void * FIFO(void *){
     
     // Simulacion
     int i;
-    for (i = 0; i < size; ++i){
+    for (i = 0; i < tamo; ++i){
         int existencia = buscarEnArreglo(s, *n, STACK);
         if (existencia == -1){
             ++fallos;
@@ -109,7 +123,7 @@ void * OPT(void *){
     // Simulacion
     int i;
     int cambio;
-    for (i = 0; i < size; ++i){
+    for (i = 0; i < tamo; ++i){
         int existencia = buscarEnArreglo(s, *n, STACK);
         if (existencia == -1){
             cambio = maximo(distancia, STACK);
@@ -133,7 +147,7 @@ void * LRU_cont(void *){
     int s[STACK];
     memset(s, -1, sizeof(int)*STACK);
     
-    int tiempo = 0;
+    int tiempo = 1;
     int contador[STACK];
     int * apunt = numeros;
     int fallos = 0;
@@ -142,8 +156,19 @@ void * LRU_cont(void *){
     
     // Simulador
     int i;
-    for (i = 0; i < size; ++i){
-        
+    for (i = 0; i < tamo; ++i){
+        int existencia = buscarEnArreglo(s, *apunt, STACK);
+        if (existencia != -1){
+            contador[existencia] = tiempo;
+        }
+        else{
+            int viejo = minimo(contador, STACK);
+            s[viejo] = *apunt;
+            contador[viejo] = tiempo;
+            fallos++;
+        }
+        ++tiempo;
+        ++apunt;
     }
     
     cout << "LRU con contador, fallos: " << fallos << endl;
@@ -156,9 +181,30 @@ void * LRU_bit(void *){
     memset(s, -1, sizeof(int)*STACK);
     
     bool bit[STACK];
-    int * apunt;
+    int * apunt = numeros;
     int fallos = 0;
     memset(bit, 0, sizeof(bool)*STACK);
+    int pos = 0;
+    
+    // Simulacion
+    for(int i = 0; i < tamo; ++i){
+        int existencia = buscarEnArreglo(s, *apunt, STACK);
+        if (existencia != -1){
+            bit[existencia] = true;
+        }
+        else{
+            while (!bit[pos]){
+                bit[pos] = false;
+                if (pos >= STACK){
+                    pos = 0;
+                }
+                ++pos;
+            }
+            bit[pos] = true;
+            s[pos] = *apunt;
+        }
+        ++apunt;
+    }
     
     cout << "LRU con un bit, fallos: " << fallos << endl;
     return 0;
@@ -173,11 +219,22 @@ int buscarEnArreglo(int * a, int n, int tamano){
     return -1;
 }
 
+int minimo(int * a, int tamano){
+    int i;
+    int min = 0;
+    for (i = 0; i < tamano; ++i){
+        if (a[i] > a[min]){
+            min = i;
+        }
+    }
+    return min;
+}
+
 int maximo(int * a, int tamano){
     int i;
     int max = 0;
     for (i = 0; i < tamano; ++i){
-        if (a[i] > a[0]){
+        if (a[i] > a[max]){
             max = i;
         }
     }
